823a28be30253b9c25e4b6cecd309714c667c0b6
pedro <pedrohgeciauskas@gmail.com>
2025-04-27 00:36:26 +0200

    Add database entities and repositories

---
+++ src/main/kotlin/com/flixfinder/model/entity/User.kt
@@ -0,0 +1,25 @@
+package com.flixfinder.model.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "user")
+data class User(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.LAZY)
+    val backlog: MutableList<UserBacklog> = mutableListOf()
+)
